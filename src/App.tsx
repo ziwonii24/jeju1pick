db5938bd4a769c6ec02d1e4663816ec212fcd83d
JJong84 <jangjwhw@gmail.com>
2021-07-11 13:23:13 +0900

    :sparkles: ADD Redux related Code
    - :sparkles: ADD store
    - :sparkles: ADD root reducer and mockup slice
    - :wrench: CHANGE esLint Configuration file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+
 import './styles/App.scss';
 
-const App = () => <div className={'hello'}>안녕하세요.</div>;
+const App = () => <div className="hello">안녕하세요.</div>;
 
 export default App;
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,3 +1,18 @@
-const reducer = 'reducer';
+import { useDispatch } from 'react-redux';
+import { ThunkDispatch } from '@reduxjs/toolkit';
+import { Action, combineReducers } from 'redux';
 
-export default reducer;
+import { IExtraArgument } from '../store';
+
+import areaSlice from './areaSlice';
+
+const rootReducer = combineReducers({
+  area: areaSlice.reducer
+});
+
+type RootState = ReturnType<typeof rootReducer>;
+const useAppDispatch = () =>
+  useDispatch<ThunkDispatch<RootState, IExtraArgument, Action>>();
+
+export { RootState, useAppDispatch };
+export default rootReducer;
